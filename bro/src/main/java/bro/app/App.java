485241e3a8e344db73e9a2513e7f49b26bec339a
tee <tee@localhost>
2025-07-24 20:57:48 +0200

    0

---
+++ bro/src/main/java/bro/app/App.java
@@ -0,0 +1,21 @@
+package bro.app;
+
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.learning.config.Sgd;
+
+public class App {
+	
+	MultiLayerConfiguration conf = new NeuralNetConfiguration().Builder()
+	        .weightInit(WeightInit.XAVIER)
+	        .activation(Activation.RELU)
+	        .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+	        .updater(new Sgd(0.05))
+	        // ... other hyperparameters
+	        .list()
+	        .backprop(true)
+	        .build();
+}
